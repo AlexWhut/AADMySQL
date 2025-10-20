7b2cd2dd54216b714b94a2ea2383c2753e8a43b0
alexwhut <bryanmoranteswt@gmail.com>
2025-10-20 10:23:48 +0200

    update

---
+++ mysql/src/main/java/id/morantesbryan/dao/Dao.java
@@ -0,0 +1,61 @@
+package id.morantesbryan.dao;
+
+import id.morantesbryan.pojos.Cliente;
+import id.morantesbryan.pojos.ClienteNuevo;
+import id.morantesbryan.pojos.LineaFactura;
+import id.morantesbryan.pojos.ResultadoListado;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class Dao {
+
+	private final Connection connection;
+
+	public Dao(Connection connection) {
+		this.connection = connection;
+	}
+
+	// Stub: insertar clientes en lote. Returns array of update counts.
+	public int[] insertarClientesEnLote(Connection conn, List<Cliente> clientes) throws SQLException {
+		// Minimal implementation: pretend each insert succeeded with 1
+		int[] results = new int[clientes.size()];
+		for (int i = 0; i < results.length; i++) results[i] = 1;
+		return results;
+	}
+
+	// Other methods used in Main: provide stubs or minimal implementations
+	public void insertarClientes(Connection conn, List<Cliente> clientes) throws SQLException {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+	public void insertarClientesBatchConTransaccion(Connection conn, List<Cliente> clientes) throws SQLException {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+	public Map<String, Integer> crearFacturas(Connection conn, List<String> dnis, List<LineaFactura> lineas) throws SQLException {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+	public ResultadoListado llamarListadoClientes(Connection conn, String dni) throws SQLException {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+	public void modificarClientesConResultSet(Connection conn, String nuevoCp, ClienteNuevo nuevoCliente) throws SQLException {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+	public void crearTablaClientesSiNoExiste() throws SQLException {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+	public void insertarDatosConStatement(Connection conn, String sql) throws SQLException {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+	public void obtenerYMostrarApellidosAlternativo(String dni, Connection conn) throws SQLException {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+}
